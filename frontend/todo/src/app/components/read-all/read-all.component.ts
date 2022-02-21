191ff9d16c5cd575fcefa47ee1b9aca3452f90fa
hvalmer <hvalmer@gmail.com>
2022-02-21 16:43:56 -0300

    read-all component

---
+++ frontend/todo/src/app/components/read-all/read-all.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/todo';
+
+@Component({
+  selector: 'app-read-all',
+  templateUrl: './read-all.component.html',
+  styleUrls: ['./read-all.component.css']
+})
+export class ReadAllComponent implements OnInit {
+
+  list: Todo[] = [
+    {
+      titulo: "Teste",
+      dataParaFinalizar: new Date,
+      finalizado: false
+    },
+    {
+      titulo: "Teste 2",
+      dataParaFinalizar: new Date,
+      finalizado: false
+    },
+    {
+      titulo: "Teste 3",
+      dataParaFinalizar: new Date,
+      finalizado: false
+    },
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
