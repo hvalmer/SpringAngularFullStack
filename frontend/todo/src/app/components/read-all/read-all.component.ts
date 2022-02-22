e4ae4274093f6ef61fc97e1ddadd597da0d538f5
hvalmer <hvalmer@gmail.com>
2022-02-22 11:00:23 -0300

    delete functionality for todo

--- frontend/todo/src/app/components/read-all/read-all.component.ts
+++ frontend/todo/src/app/components/read-all/read-all.component.ts
@@ -33,4 +33,15 @@ export class ReadAllComponent implements OnInit {
      this.closed = this.listFinished.length;
     })
   }
+
+  delete(id: any):void{
+    this.service.delete(id).subscribe((resposta) => {
+      if(resposta === null){
+        this.service.message('Task deletada com sucesso!!');
+        this.list = this.list.filter(todo => todo.id !== id);
+      }
+    })
+  }
 }
+
+
