8c9fe08d499367567214ae346aea74727fe0e3fc
hvalmer <hvalmer@gmail.com>
2022-02-21 23:07:00 -0300

    badge functionality

--- frontend/todo/src/app/app.module.ts
+++ frontend/todo/src/app/app.module.ts
@@ -12,13 +12,14 @@ import { ReadAllComponent } from './components/read-all/read-all.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ReadAllComponent    
+    ReadAllComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +28,8 @@ import { MatIconModule } from '@angular/material/icon';
     MatCardModule,
     MatButtonModule,
     MatIconModule,
-    HttpClientModule
+    HttpClientModule,
+    MatBadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
